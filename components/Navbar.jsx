97b161b69a6ccded63ca88a1907423454d2401de
Rubaisa <rubaisajawed8@gmail.com>
2023-02-23 23:07:49 +0500

    minor edit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,31 +6,31 @@ import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
-<motion.nav
-  variants = {navVariants}
-  initial = "hidden"
-  whileInView = "show"
-  className = {`${styles.xPaddings} py-8 relative`}
->
-  <div className="absolute w-[50%] inset-0 gradient-01" />
-  <div className = {`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-    <img
-      src="/search.svg"
-      alt="search"
-      className='w-[24px] h-[24px] object-contain'
-    />
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
 
-    <h2 className="font-extrabold text-[24px] loading-[30px] text-white">
-      DECYPHER LABS
-    </h2>
+      <h2 className="font-extrabold text-[24px] loading-[30px] text-white">
+        DECYPHER LABS
+      </h2>
 
-    <img 
-    src="./menu.svg"
-    alt="menu"
-    className="w-[24px] h-[24px] object-contain" />
-
-  </div>
-</motion.nav>
+      <img
+        src="./menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
